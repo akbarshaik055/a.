86fab89c2958cd7ebff916154657c0c1152ed04e
akbarshaik055 <42571502+akbarshaik055@users.noreply.github.com>
2019-02-19 13:12:59 +0530

    Update a8.py

--- a8.py
+++ a8.py
@@ -5,14 +5,12 @@ import math
 # number is semiprime or not 
 def checkSemiprime(num): 
     cnt = 0
-  
-    for i in range(2, int(math.sqrt(num)) + 1): 
+  for i in range(2, int(math.sqrt(num)) + 1): 
         while num % i == 0: 
             num /= i 
             cnt += 1 # Increment count 
                     # of prime number 
-  
-        # If count is greater than 2, 
+   # If count is greater than 2, 
         # break loop  
         if cnt >= 2:  
             break
@@ -21,22 +19,18 @@ def checkSemiprime(num):
     # number remain is prime number 
     if(num > 1): 
         cnt += 1
-  
     # Return '1' if count is equal to '2' else 
     # return '0' 
     return cnt == 2
-  
-# Function to print 'True' or 'False' 
+  # Function to print 'True' or 'False' 
 # according to condition of semiprime 
 def semiprime(n): 
     if checkSemiprime(n) == True: 
         print("True") 
     else: 
         print("False") 
-  
-# Driver code 
+  # Driver code 
 n = 6
 semiprime(n) 
-  
-n = 8
+  n = 8
 semiprime(n); 
